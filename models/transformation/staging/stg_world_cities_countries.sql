ea6ced72ab14948c517516e6dee11197bef96f9b
Isin Pesch <isin.pesch@deel.com>
2024-08-13 07:48:57 +0000

    remove stg

--- models/transformation/staging/stg_world_cities_countries.sql
+++ models/transformation/staging/stg_world_cities_countries.sql
@@ -11,7 +11,7 @@ with final as (
 
     from {{ ref('world_cities_countries') }} as c
     left join
-        {{ ref('stg_common_english_words') }} sw -- Assume 'word' is the column name in 'stg_word'
+       {{ ref('common_english_words') }} sw -- Assume 'word' is the column name in 'stg_word'
     on
         lower(c.city_name_latin) = lower(sw.word)
     where
