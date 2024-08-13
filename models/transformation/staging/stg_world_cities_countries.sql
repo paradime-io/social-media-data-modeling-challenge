ffaaede6b029515ae4cbdf4cbda023adafb0ad10
Isin Pesch <isin.pesch@deel.com>
2024-08-13 15:23:23 +0000

    add docs world_cities_countries

--- models/transformation/staging/stg_world_cities_countries.sql
+++ models/transformation/staging/stg_world_cities_countries.sql
@@ -3,6 +3,7 @@
 with final as (
     select
         -- Strings
+        {{ dbt_utils.generate_surrogate_key(['c.city_name_original', 'c.country_name']) }} as unique_key,
         c.city_name_original,
         c.city_name_latin,
         c.country_name,
@@ -11,7 +12,7 @@ with final as (
 
     from {{ ref('world_cities_countries') }} as c
     left join
-       {{ ref('common_english_words') }} sw -- Assume 'word' is the column name in 'stg_word'
+       {{ ref('common_english_words') }} sw
     on
         lower(c.city_name_latin) = lower(sw.word)
     where
