6206f5ab2559d7b6f86b396a3213aa4de7e2e071
Mads Christensen <msc@playable.com>
2024-08-29 20:52:44 +0000

    delete all models - I regretted the source, too weak

--- models/staging/stg_author_status.sql
+++
@@ -1,9 +0,0 @@
-WITH base AS (
-SELECT
-        video_id,
-        verified_status,
-        author_ban_status
-FROM {{source('main', 'tiktok_dataset')}}
-)
-
-SELECT * FROM base 
--- models/staging/stg_video_metadata.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-    video_id,
-    video_duration_sec,
-    verified_status,
-    author_ban_status,
-    claim_status
-FROM {{source('main', 'tiktok_dataset')}}
