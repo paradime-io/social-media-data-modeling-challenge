82e51f39570366cac4be4a770b2f699cf4e15b87
Mads Christensen <msc@playable.com>
2024-08-30 22:06:28 +0000

    create dim tiktok table excluding music data

---
+++ models/marts/dim_tiktok_videos.sql
@@ -0,0 +1,23 @@
+with base as (
+    select
+        video_id,
+        caption,
+        created_at,
+        likes_count,
+        shares_count,
+        play_count,
+        comment_count,
+        is_downloaded,
+        author_name,
+        author_nickname,
+        author_verified,
+        author_signature,
+        video_height,
+        video_width,
+        video_duration
+    from {{ ref('stg_tiktok_videos') }}
+)
+
+select * from base
+  
+
