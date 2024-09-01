fda91bea08533f926a421b665df3dd0cc85af1a3
Jayeson Gao <jayeson.gao@instacart.com>
2024-09-01 06:50:56 +0000

    small tweaks to add_genre.py

--- scripts/add_genre.py
+++ scripts/add_genre.py
@@ -22,7 +22,7 @@ X = df_train.drop(['track_name', 'track_artist', 'genre', 'subgenre', 'danceabil
 y = df_train['genre']
 
 # Splitting the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify = y)
 
 # Setting up and training the Random Forest Classifier
 model = RandomForestClassifier(random_state=42)
