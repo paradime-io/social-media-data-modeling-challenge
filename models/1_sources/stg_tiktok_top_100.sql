d57848ef99e5e321ab40c6a88452ef807a555dd6
Jayeson Gao <jayeson.gao@instacart.com>
2024-09-01 01:23:34 +0000

    update field names "track name / track author"

--- models/1_sources/stg_tiktok_top_100.sql
+++ models/1_sources/stg_tiktok_top_100.sql
@@ -3,8 +3,8 @@ with source as (
         CASE WHEN _source LIKE 'https%' THEN
             DATE_TRUNC('month', STRPTIME(SUBSTRING(_source, INSTR(_source, 'months/') + 7), '%B-%Y'))
         END as date
-        , title
-        , artist
+        , title as track_name
+        , artist as track_author
         , rank as rank_by_videos
         , views
         , popularVideos as viral_video_count
