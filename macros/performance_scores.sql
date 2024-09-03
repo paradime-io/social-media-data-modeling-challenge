43c1bd028b9ed938edb4ced751608df046afc135
Jayeson Gao <jayeson.gao@instacart.com>
2024-09-03 17:42:49 +0000

    update performance score weights

--- macros/performance_scores.sql
+++ macros/performance_scores.sql
@@ -5,9 +5,9 @@
     0.15 * {{ avg_top50_rank }} +
     0.15 * {{ avg_top10_rank }} AS base_performance_score,
     0.1 * {{ total }} +
-    0.35 * {{ top50_count }} +
+    0.5 * {{ top50_count }} +
     0.1 * {{ top10_count }} +
-    0.35 * {{ avg_top50_rank }} +
+    0.2 * {{ avg_top50_rank }} +
     0.1 * {{ avg_top10_rank }} AS consistency_score,
     0.1 * {{ total }} +
     0.1 * {{ top50_count }} +
