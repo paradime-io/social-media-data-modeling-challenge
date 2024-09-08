efb65d707742a4c708c4cd71c2c3d08217485186
Demik Freitas <demik.freitast2d18@gmail.com>
2024-09-08 21:05:25 +0000

    Feat: Intermediate level - Keywords analysis

---
+++ models/intermediate/int_slashdot_keywords.sql
@@ -0,0 +1,20 @@
+WITH keywords_occurences AS (
+    SELECT 
+        UPPER(technologyKeys) AS technologyKeys,
+        title
+    FROM {{ ref('stg_slashdot') }}
+    CROSS JOIN {{ ref('technologyKeywords') }}
+    WHERE UPPER(title) LIKE CONCAT('% ', UPPER(technologyKeys), ' %')
+),
+total_keywords AS (
+    SELECT 
+        technologyKeys,
+        COUNT(*) AS ocurrences
+    FROM keywords_occurences
+    GROUP BY technologyKeys
+    ORDER BY ocurrences
+)
+SELECT 
+    *
+FROM total_keywords
+ORDER BY ocurrences DESC
