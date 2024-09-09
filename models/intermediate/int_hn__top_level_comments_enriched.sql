f8162748fc1d3910c95031cf0f823cf068da3eba
Sophie Li <s.li@obieinsurance.com>
2024-09-09 03:02:18 +0000

    cleanup

--- models/intermediate/int_hn__top_level_comments_enriched.sql
+++
@@ -1,7 +0,0 @@
-select
-    stories.*,
-    top_level_comments.*
-from {{ ref('int_hn__top_level_comments') }} as top_level_comments
-left join
-    {{ ref('stg_hn__stories') }} as stories
-    on top_level_comments.parent_story_id = stories.id
